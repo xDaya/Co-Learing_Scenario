b0c6bd1ec2cd117d128d723e2968897c10f1d5c9
xdaya <emmavanzoelen@gmail.com>
2024-02-08 12:16:37 +0100

    Made sure to exclude objects out of field from break/pick up

--- robot_partner.py
+++ robot_partner.py
@@ -79,7 +79,7 @@ class RobotPartner(AgentBrain):
 
         self.condition = 2
 
-        self.exp_condition = 'ontology'
+        self.exp_condition = 'communication'
 
         self.database_name = None
 
